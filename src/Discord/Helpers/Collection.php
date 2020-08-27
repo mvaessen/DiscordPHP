4c472d6a8123aa5bc2553f8653d27cac4c0a4ade
Max Vaessen <maxvaessen@gmail.com>
2020-08-27 15:59:52 +0200

    make Collection helper compatible with Illuminate/Support 6.x and below + update composer requirements

--- src/Discord/Helpers/Collection.php
+++ src/Discord/Helpers/Collection.php
@@ -30,34 +30,6 @@ class Collection extends BaseCollection
     }
 
     /**
-     * {@inheritdoc}
-     */
-    public function push(...$values)
-    {
-        foreach ($values as $value) {
-            $this->offsetSet(null, $value);
-        }
-
-        return $this;
-    }
-
-    /**
-     * Fills the collection with the given array.
-     *
-     * @param array $items Items to fill.
-     */
-    public function fill($items = [])
-    {
-        $this->items = [];
-
-        foreach ($items as $item) {
-            $this->offsetSet(null, $item);
-        }
-
-        return $this;
-    }
-
-    /**
      * Get an item from the collection with a key and value.
      *
      * @param mixed $key   The key to match with the value.
@@ -85,29 +57,6 @@ class Collection extends BaseCollection
     }
 
     /**
-     * Gets the index of an item in the collection.
-     *
-     * @param mixed $key   The key to match with the value.
-     * @param mixed $value The value to match with the key.
-     *
-     * @return mixed The value or null.
-     */
-    public function getIndex($key, $value = null)
-    {
-        foreach ($this->items as $index => $item) {
-            if (is_array($item)) {
-                if ($item[$key] == $value) {
-                    return $index;
-                }
-            } elseif (is_object($item)) {
-                if ($item->{$key} == $value) {
-                    return $index;
-                }
-            }
-        }
-    }
-
-    /**
      * Gets a collection of items from the repository with a key and value.
      *
      * @param mixed $key   The key to match with the value.
@@ -117,7 +66,7 @@ class Collection extends BaseCollection
      */
     public function getAll($key, $value = null)
     {
-        $collection = new self([], $this->discrim);
+        $collection = new self();
 
         foreach ($this->items as $item) {
             if ($item->{$key} == $value) {
@@ -133,23 +82,21 @@ class Collection extends BaseCollection
      */
     public function offsetSet($key, $value)
     {
-        if (! is_null($key)) {
-            $this->items[$key] = $value;
-
-            return $this;
-        }
-
         if (! is_null($this->discrim)) {
-            if (is_array($value)) {
-                $this->items[$value[$this->discrim]] = $value;
-            } elseif (is_object($value)) {
+            if (! is_array($value)) {
                 $this->items[$value->{$this->discrim}] = $value;
+            } else {
+                $this->items[$value[$this->discrim]] = $value;
             }
-        } else {
-            $this->items[] = $value;
+
+            return;
         }
 
-        return $this;
+        if (is_null($key)) {
+            $this->items[] = $value;
+        } else {
+            $this->items[$key] = $value;
+        }
     }
 
     /**
